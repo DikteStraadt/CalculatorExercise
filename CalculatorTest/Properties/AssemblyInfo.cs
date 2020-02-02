a2fc6bc9949084bf2c32737af7493225392d21dc
Dikte Straadt <Dikte.straadt@hotmail.com>
2020-02-02 19:03:23 +0100

    Add project files.

---
+++ CalculatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f5f9efbe-885f-4d12-9581-32e324320bae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalculatorTest/UnitTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using CalculatorExercise;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        private Calculator uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            uut = new Calculator();
+        }
+
+        [TestCase(1,2,3)]
+        [TestCase(-1, 2, 1)]
+        [TestCase(-1, -2, -3)]
+        public void Test_Add(double a, double b, double output)
+        {
+            Assert.That(uut.Add(a,b), Is.EqualTo(output));
+        }
+
+        [TestCase(1, 2, -1)]
+        [TestCase(-1, 2, -3)]
+        [TestCase(-1, -2, 1)]
+        public void Test_Substract(double a, double b, double output)
+        {
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(output));
+        }
+
+        [TestCase(1, 2, 2)]
+        [TestCase(-1, 2, -2)]
+        [TestCase(-1, -2, 2)]
+        public void Test_Multiply(double a, double b, double output)
+        {
+            Assert.That(uut.Multiply(a, b), Is.EqualTo(output));
+        }
+
+        [TestCase(1, 2, 1)]
+        [TestCase(-1, 2, 1)]
+        [TestCase(-1, -2, 1)]
+        public void Test_Power(double x, double exp, double output)
+        {
+            Assert.That(uut.Power(x, exp), Is.EqualTo(output));
+        }
+    }
+}
